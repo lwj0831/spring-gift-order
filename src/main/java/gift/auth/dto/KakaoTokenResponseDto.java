3a99de05aab1e087bb856ee7d7bd365624aa98d3
lwj0831 <wjl0831@gmail.com>
2025-07-25 21:18:51 +0900

    feat(auth): 카카오 외부 API 호출 담당하는 KakaoApiClient 구현

---
+++ src/main/java/gift/auth/dto/KakaoTokenResponseDto.java
@@ -0,0 +1,14 @@
+package gift.auth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record KakaoTokenResponseDto(
+    String tokenType,
+    String accessToken,
+    Integer expiresIn,
+    String refreshToken,
+    Integer refreshTokenExpiresIn,
+    String scope
+) {}
