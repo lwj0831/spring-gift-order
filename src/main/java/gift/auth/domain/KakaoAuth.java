8060f00c0e987027dff795b8aea21ad240eba9a4
lwj0831 <wjl0831@gmail.com>
2025-07-29 19:24:47 +0900

    feat(auth): KakaoAuth 엔티티 정의 및 KakaoAuthJpaRepository클래스 추가
    - KakaoAuth엔티티 : 카카오 엑세스 토큰, 리프레쉬 토큰, 만료 시간 정보 포함
    - KakaoAuthJpaRepository : dirtyChecking 대신 Update쿼리 사용

---
+++ src/main/java/gift/auth/domain/KakaoAuth.java
@@ -0,0 +1,64 @@
+package gift.auth.domain;
+
+import gift.global.common.jpa.TimeBaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "kakao_auth")
+public class KakaoAuth extends TimeBaseEntity {
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String accessToken;
+    @Column(nullable = false)
+    private String refreshToken;
+    @Column(nullable = false)
+    private LocalDateTime accessTokenExpiresAt;
+    @Column(nullable = false)
+    private LocalDateTime refreshTokenExpiresAt;
+
+    protected KakaoAuth() {
+    }
+
+    private KakaoAuth(Long id, String accessToken, String refreshToken, Integer accessExpiresIn, Integer refreshExpiresIn) {
+        this.id = id;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        LocalDateTime now = LocalDateTime.now();
+        this.accessTokenExpiresAt = now.plusSeconds(accessExpiresIn);
+        this.refreshTokenExpiresAt = now.plusSeconds(refreshExpiresIn);
+    }
+
+    public static KakaoAuth withId(Long id, String accessToken, String refreshToken, Integer accessExpiresIn, Integer refreshExpiresIn){
+        return new KakaoAuth(id,accessToken, refreshToken, accessExpiresIn, refreshExpiresIn);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void updateTokenInfo(String accessToken, String refreshToken){
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public boolean isAccessTokenExpired() {
+        return LocalDateTime.now().isAfter(accessTokenExpiresAt);
+    }
+
+    public boolean isRefreshTokenExpired() {
+        return LocalDateTime.now().isAfter(refreshTokenExpiresAt);
+    }
+}
