b97aec726f70c7e023acb7be9467bc992ab21ef5
lwj0831 <wjl0831@gmail.com>
2025-07-29 19:40:01 +0900

    feat(auth): KakaoOauthService에 KakaoAuth 관련 로직 추가

--- src/main/java/gift/auth/service/KakaoOauthService.java
+++ src/main/java/gift/auth/service/KakaoOauthService.java
@@ -1,19 +1,21 @@
 package gift.auth.service;
 
 import gift.auth.config.KakaoOauthProperties;
+import gift.auth.domain.KakaoAuth;
 import gift.auth.domain.MemberAuth;
 import gift.auth.domain.TokenInfo;
 import gift.auth.dto.KakaoTokenResponseDto;
 import gift.auth.dto.KakaoUserInfoResponseDto;
 import gift.auth.dto.LoginResponseDto;
-import gift.auth.dto.RegisterMemberResponseDto;
-import gift.auth.exception.DuplicatedEmailException;
+import gift.auth.repository.KakaoAuthJpaRepository;
 import gift.auth.repository.MemberAuthJpaRepository;
 import gift.auth.utils.EmailUtils;
 import gift.member.domain.Member;
 import gift.member.domain.MemberType;
 import gift.member.repository.MemberJpaRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
@@ -21,41 +23,68 @@ public class KakaoOauthService {
 
     private final MemberAuthJpaRepository memberAuthJpaRepository;
     private final MemberJpaRepository memberJpaRepository;
+    private final KakaoAuthJpaRepository kakaoAuthJpaRepository;
     private final KakaoApiClient kakaoApiClient;
     private final TokenService tokenService;
+    private final KakaoTokenService kakaoTokenService;
     private final KakaoOauthProperties properties;
 
     public KakaoOauthService(MemberAuthJpaRepository memberAuthJpaRepository,
-        MemberJpaRepository memberJpaRepository, KakaoApiClient kakaoApiClient,
-        TokenService tokenService, KakaoOauthProperties properties) {
+        MemberJpaRepository memberJpaRepository, KakaoAuthJpaRepository kakaoAuthJpaRepository,
+        KakaoApiClient kakaoApiClient, TokenService tokenService,
+        KakaoTokenService kakaoTokenService,
+        KakaoOauthProperties properties) {
         this.memberAuthJpaRepository = memberAuthJpaRepository;
         this.memberJpaRepository = memberJpaRepository;
+        this.kakaoAuthJpaRepository = kakaoAuthJpaRepository;
         this.kakaoApiClient = kakaoApiClient;
         this.tokenService = tokenService;
+        this.kakaoTokenService = kakaoTokenService;
         this.properties = properties;
     }
 
-    public LoginResponseDto loginKakaoUser(String authorizationCode){
+    @Transactional
+    public LoginResponseDto loginKakaoMember(String authorizationCode) {
         KakaoTokenResponseDto tokenResponse = kakaoApiClient.getAccessToken(authorizationCode);
         KakaoUserInfoResponseDto userInfo = kakaoApiClient.getUserInfo(tokenResponse.accessToken());
 
         //비즈니스 정책에 따른 이메일 생성
         String email = EmailUtils.createEmailByKakaoId(userInfo.id());
 
-        //이메일 중복 검증
-        if(memberAuthJpaRepository.findByEmail(email).isPresent()){
-            throw new DuplicatedEmailException();
-        }
+        //기존 카카오 소셜 회원 존재 여부 검증
+        Optional<MemberAuth> memberAuth = memberAuthJpaRepository.findByEmail(email);
+        Long memberId;
 
-        Member member = Member.of(userInfo.properties().nickname(), MemberType.KAKAO);
+        //존재하면 로그인
+        if (memberAuth.isPresent()) {
+            memberId = memberAuth.get().getId();
+            kakaoTokenService.updateKakaoTokenInfo(memberId, tokenResponse.accessToken(),
+                tokenResponse.refreshToken());
+        }
+        //존재안하면 회원가입
+        else {
+            memberId = registerKakaoMember(userInfo.properties().nickname(), email,
+                tokenResponse.accessToken(), tokenResponse.refreshToken(),
+                tokenResponse.expiresIn(), tokenResponse.refreshTokenExpiresIn());
+        }
+        //서비스 자체 JWT발급
+        TokenInfo tokenInfo = tokenService.generateAndUpdateBearerTokenInfo(memberId, email);
+        return LoginResponseDto.from(tokenInfo);
+    }
+
+    private Long registerKakaoMember(String nickName, String email, String kakaoAccessToken,
+        String kakaoRefreshToken,
+        Integer expiresIn, Integer refreshExpiresIn) {
+        Member member = Member.of(nickName, MemberType.KAKAO);
         Long memberId = memberJpaRepository.save(member).getId();
 
         MemberAuth memberAuth = MemberAuth.withId(memberId, email, "{noop}SOCIAL_LOGIN_USER");
         memberAuthJpaRepository.save(memberAuth);
 
-        //서비스 자체 JWT발급
-        TokenInfo tokenInfo = tokenService.generateBearerTokenInfo(memberId, email);
-        return LoginResponseDto.from(tokenInfo);
+        KakaoAuth kakaoAuth = KakaoAuth.withId(memberId, kakaoAccessToken, kakaoRefreshToken,
+            expiresIn, refreshExpiresIn);
+        kakaoAuthJpaRepository.save(kakaoAuth);
+        return memberId;
     }
 
     public String createAuthorizeRedirectUrl() {
